540e89354a400bc8728ea45dec190b6e3e51df9a
hammad-iftikhar <hammad.iftikhar723@gmail.com>
2026-04-09 02:18:42 +0500

    chore: project setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function App() {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <Button>Habu</Button>
+    </div>
+  );
+}
